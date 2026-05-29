da3062ee97f4c7387ef3ea7d28393ab47acccb14
anhtuandev-04 <tranhtuan98@gmail.com>
2026-05-29 09:00:53 +0700

    Update check.py

--- check.py
+++ check.py
@@ -2,13 +2,13 @@ import asyncio
 import aiohttp
 import time
 import logging
-from aiohttp import TCPConnector
+from aiohttp import TCPConnector 
 
 CONCURRENT_REQUESTS = 50
 
 CONNECTION_LIMIT = 100
 
-BATCH_SIZE = 1000 
+BATCH_SIZE = 1000
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s', handlers=[
     logging.FileHandler("log.log"),
